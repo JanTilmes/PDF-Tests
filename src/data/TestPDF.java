09c8a5493414fc7f0a359847e5d154131f1d8750
Jan Tilmes Mac <jan.p.tilmes@gmail.com>
2017-12-06 19:41:55 +0100

    Initial Commit

---
+++ src/data/TestPDF.java
@@ -0,0 +1,41 @@
+package data;
+
+import java.io.File;
+import java.net.URL;
+
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDTrueTypeFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+public class TestPDF {
+
+	public static void main(String[] args) {
+		File file = new File("src/pdfs/BEFUNDSCHEIN.pdf");
+		PDDocument document = null;
+		try {
+			document = PDDocument.load(file);
+			PDPage page1 = document.getPage(0);
+			
+			PDPageContentStream contentStream = new PDPageContentStream(document, page1, PDPageContentStream.AppendMode.APPEND, false);
+			
+			contentStream.setFont(PDType1Font.HELVETICA, 11);
+			 contentStream.beginText();
+			  contentStream.newLineAtOffset(483,664);
+			  contentStream.showText("123456");
+			  contentStream.endText();
+			  contentStream.close();
+
+			document.save("../test.pdf");
+			System.out.println("PDF erstellt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				document.close();
+			} catch (Exception ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+	}
+}
